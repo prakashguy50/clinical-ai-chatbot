941a44089ef29abe08d16daa8afa6d97c3624463
Prakash Arjunan <prakashguy50@gmail.com>
2025-04-01 16:13:55 +0000

    updated the vertex ai service conffigs

--- app/vertex_ai_service.py
+++ app/vertex_ai_service.py
@@ -5,7 +5,7 @@ import os
 PROJECT_ID = os.getenv("GCP_PROJECT_ID", "healthvision-ml-dev")
 REGION = os.getenv("GCP_REGION", "us-central1")
 
-vertexai.init(project="healthvision-ml-dev", location="us-central1")
+vertexai.init(project=PROJECT_ID, location=REGION)
 model = TextGenerationModel.from_pretrained("text-bison")
 
 def get_vertexai_response(prompt: str) -> str:
